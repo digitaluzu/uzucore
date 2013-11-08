14f4aa51ae15164ccbd80997c609e6f0c949f3d4
Shyam Guthikonda <shyamguth@gmail.com>
2013-11-08 23:13:48 +0900

    Update.

--- Prefs.cs
+++ Prefs.cs
@@ -17,29 +17,49 @@ namespace Uzu
 			return PlayerPrefs.GetFloat (key, defaultValue);
 		}
 		
+		public static void SetFloat (string key, float val)
+		{
+			PlayerPrefs.SetFloat (key, val);
+		}
+		
 		public static int GetInt (string key, int defaultValue)
 		{
 			return PlayerPrefs.GetInt (key, defaultValue);
+		}
+		
+		public static void SetInt (string key, int val)
+		{
+			PlayerPrefs.SetInt (key, val);
 		}
 		
 		public static string GetString (string key, string defaultValue)
 		{
 			return PlayerPrefs.GetString (key, defaultValue);
 		}
-		
-		public static void SetFloat (string key, float val)
-		{
-			PlayerPrefs.SetFloat (key, val);
-		}
-	
-		public static void SetInt (string key, int val)
-		{
-			PlayerPrefs.SetInt (key, val);
-		}
 	
 		public static void SetString (string key, string val)
 		{ 
 			PlayerPrefs.SetString (key, val);
+		}
+		
+		public static bool GetBool (string key, bool defaultValue)
+		{
+			return PlayerPrefs.GetInt (key, defaultValue ? 1 : 0) == 1;
+		}
+		
+		public static void SetBool (string key, bool val)
+		{
+			PlayerPrefs.SetInt (key, val ? 1 : 0);
+		}
+		
+		public static bool HasKey (string key)
+		{
+			return PlayerPrefs.HasKey (key);
+		}
+	
+		public static void DeleteKey (string key)
+		{
+			PlayerPrefs.DeleteKey (key);
 		}
 		
 		/// <summary>
